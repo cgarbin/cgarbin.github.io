0f9644ad0a4a8bcd08ed4e793c2c20daa01957d1
Christian Garbin <garbin.christian@gmail.com>
2021-04-09 22:07:26 -0400

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+# Publish the site to the GitHub pages branch
 
 echo "Remember to update the local page:"
 echo "  bundle exec jekyll serve"
 
+set -xe
+
 mv _site ../_site-ghio
 git checkout -B gh-pages
-rm -rf *
-mv ../_site-ghio _site
+rm -rf -- *
+mv ../_site-ghio/* .
 git add .
 git commit -m "Update site"
 git push origin gh-pages --force
 git checkout master
+rm -rf ../_site-ghio
