c50e07b924b512c06a6f31cc8a47199f98acc4bc
Christian Garbin <garbin.christian@gmail.com>
2022-10-01 07:59:37 -0400

    Add more checks to publish the correct version

--- publish.sh
+++ publish.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 # Publish the site to the GitHub pages branch, making it public
 
-# This didn't quite work -- needs more testing
-# export JEKYLL_ENV=production
-# jekyll build
-
+# Do no publish uncommited changes
 if [ -n "$(git status --porcelain)" ]; then
     echo ""
     echo "There are uncommited changes - commit or stash them first"
     exit 1
 fi
+
+# Verify that we are not serving locally
+# It will override the published site eventually, perhaps even in the middle of publishing
+if pgrep -f "jekyll serve" > /dev/null; then
+    echo ""
+    echo "Jekyll is serving locally - stop it first"
+    exit 1
+fi
+
+# Verify that we are not trying to publish a test version
+#   Test version: bundle exec jekyll serve       <-- we don't want this
+#   Production version: bundle exec jekyll build <-- we want this
+# The test version uses "localhost" in the links
+cd _site
+if grep -Rq --include="*.html" localhost; then
+    echo ""
+    echo "This is a test version - use 'bundle exec jekyll build' to publish"
+    exit 1
+fi
+cd ..
+
+exit 0
 
 set -xe
 
